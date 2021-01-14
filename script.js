68613ec0fa6ace1983641e8c577fd97718c0345e
Thomas Nunes <nds.thomas@gmail.com>
2021-01-14 17:38:20 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
 
+const keySequence = [];
+let konamiString = '';
+const konamiCode = [
+  'ArrowUp',
+  'ArrowUp',
+  'ArrowDown',
+  'ArrowDown',
+  'ArrowLeft',
+  'ArrowRight',
+  'ArrowLeft',
+  'ArrowRight',
+  'b',
+  'a'
+];
+
+document.addEventListener('keydown', function(e) {
+  // To make sure it freezes the scroll when 
+  // the first two keypresses are "ArrowUp"
+  if (keySequence[0] === 'ArrowUp' && keySequence[1] === 'ArrowUp' && e.key === 'ArrowDown') {
+    e.preventDefault();
+  }
+});
+
+document.addEventListener('keyup', function(e) {
+  const doc = document.documentElement;
+
+    keySequence.push(e.key);
+    keySequence.splice(-konamiCode.length - 1, keySequence.length - konamiCode.length);
+    konamiString = konamiCode.join('');
+
+    if (keySequence.join('').includes(konamiString)) {
+      document.getElementById("nds-header-logo").style.transform = "rotate(90deg)";
+      document.getElementById("nds-header-logo").style.background = "#AAA"
+    }
+  
+});
